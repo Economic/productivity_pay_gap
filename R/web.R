dd9866e408b390b746b32daad362c14b7b873d31
Ben Zipperer <benzipperer@gmail.com>
2024-09-18 10:07:28 -0400

    add average annual growth rates

--- R/web.R
+++ R/web.R
@@ -34,7 +34,9 @@ create_web_stats_csv <- function(data, file) {
     mutate(quarter_date = paste0(year(quarter_date), "q", quarter(quarter_date))) |> 
     pull(quarter_date)
   
-  pct_growth_1979q4_present = data |> 
+  max_year = year(yq(max_quarter_date))
+  
+  total_growth_1979q4_present = data |> 
     filter(quarter_date == yq("1979q4") | quarter_date == yq(max_quarter_date)) |> 
     arrange(quarter_date) |> 
     mutate(value = value / lag(value) - 1, .by = name) |> 
@@ -44,9 +46,9 @@ create_web_stats_csv <- function(data, file) {
       "comp_private_real_index_1948" ~ "Productivity, 1979q4–",
       "productivity_real_index_1948" ~ "Hourly pay, 1979q4–"
     )) |> 
-    mutate(name = paste0(name, max_quarter_date, ", percent growth"))
+    mutate(name = paste0(name, max_quarter_date, ", total percent growth"))
   
-  ratio = pct_growth_1979q4_present |> 
+  ratio = total_growth_1979q4_present |> 
     summarize(value = max(value) / min(value)) |> 
     mutate(
       value = label_number(accuracy = 0.1)(value),
@@ -55,13 +57,58 @@ create_web_stats_csv <- function(data, file) {
     ) |> 
     select(name, value)
   
-  pct_growth_1979q4_present = pct_growth_1979q4_present |> 
+  total_growth_1979q4_present = total_growth_1979q4_present |> 
     mutate(value = label_percent(accuracy = 0.1)(value)) |> 
     select(name, value)
   
+  avg_growth_1948_1979 = data |> 
+    arrange(name, quarter_date) |> 
+    mutate(time = row_number(), .by = name) |> 
+    filter(quarter_date %in% yq(c("1948q1", "1979q4"))) |>
+    mutate(
+      total_growth = value / lag(value) - 1,
+      total_years = (time - lag(time) + 1) / 4,
+      .by = name
+    ) |> 
+    filter(!is.na(total_growth)) |> 
+    mutate(
+      value = (total_growth + 1)^(1/total_years) - 1,
+      value = label_percent(accuracy = 0.1)(value)
+    ) |> 
+    mutate(name = case_match(
+      name,
+      "comp_private_real_index_1948" ~ "Productivity, 1948–1979, average annual growth",
+      "productivity_real_index_1948" ~ "Compensation, 1948–1979, average annual growth"
+    )) |> 
+    select(name, value)
+  
+  avg_growth_1979_present = data |> 
+    arrange(name, quarter_date) |> 
+    mutate(time = row_number(), .by = name) |> 
+    filter(quarter_date %in% yq(c("1979q4", max_quarter_date))) |>
+    mutate(
+      total_growth = value / lag(value) - 1,
+      total_years = (time - lag(time) + 1) / 4,
+      .by = name
+    ) |> 
+    filter(!is.na(total_growth)) |> 
+    mutate(
+      value = (total_growth + 1)^(1/total_years) - 1,
+      value = label_percent(accuracy = 0.1)(value)
+    ) |> 
+    mutate(name = case_match(
+      name,
+      "comp_private_real_index_1948" ~ "Productivity, 1979–",
+      "productivity_real_index_1948" ~ "Compensation, 1979–"
+    )) |> 
+    mutate(name = paste0(name, max_year, ", average annual growth")) |> 
+    select(name, value)
+  
   output = bind_rows(
-    pct_growth_1979q4_present,
-    ratio
+    total_growth_1979q4_present,
+    ratio,
+    avg_growth_1948_1979,
+    avg_growth_1979_present
   ) |> 
     select(name, value)
   
