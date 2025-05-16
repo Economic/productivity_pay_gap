7d5502d92dfe10871ecfefd9bd16916ad86bd09a
Ben Zipperer <benzipperer@gmail.com>
2025-05-15 23:21:49 -0400

    add more sources documentation

--- R/document_data.R
+++ R/document_data.R
@@ -12,44 +12,45 @@ document_data = function(
     source = "BLS Office of Productivity and Technology",
     source_url = "https://www.bls.gov/productivity/tables/home.htm",
     source_url_2 = "https://www.bls.gov/productivity/tables/total-economy-hours-employment.xlsx",
+    access_method = "manual download",
     data_location = bls_hours
   )
 
-  bls_current_wages_doc = tibble(
-    name = "CES production and nonsupervisory average hourly earnings, private-sector",
-    source = "BLS Current Employment Statistics",
-    source_url = "https://www.bls.gov/developers/home.htm"
-  ) |>
-    mutate(date_accessed = api_download_date)
+  bls_early_wages_doc = tibble(
+    name = "CES production average hourly earnings, private-sector, not seasonally adjusted",
+    date_accessed = ymd("2025 May 15"),
+    source = "BLS Current Employment Statistics, series EEU00500006",
+    source_url = "https://data.bls.gov/series-report",
+    access_method = "manual_download",
+    data_location = bls_early_wages
+  )
+
+  bls_current_sa = tibble(
+    name = "CES production and nonsupervisory average hourly earnings, private-sector, seasonally adjusted",
+    source = "BLS Current Employment Statistics, series CES0500000008"
+  )
+
+  bls_current_nsa = tibble(
+    name = "CES production and nonsupervisory average hourly earnings, private-sector, not seasonally adjusted",
+    source = "BLS Current Employment Statistics, series CEU0500000008"
+  )
+
+  bls_current_wages_doc = bls_current_sa |>
+    bind_rows(bls_current_nsa) |>
+    mutate(
+      source_url = "https://www.bls.gov/developers/home.htm",
+      access_method = "BLS API",
+      data_location = bls_current_wages,
+      date_accessed = api_download_date
+    )
 
   bind_rows(
     bls_hours_doc,
     bls_current_wages_doc,
+    bls_early_wages_doc
   ) |>
     create_csv(output_file)
 }
-
-# bls_hours_doc = document_bls_hours(bls_hours_xlsx) |>
-#     tar_target()
-#   bls_wages_doc = document_bls_wages(
-#     early = bls_early_wages_csv,
-#     current = bls_series_csv
-#   ) |>
-#     tar_target()
-#   bea_data_doc = document_bea_data(bea_series_csv) |>
-#     tar_target()
-
-# document_bls_hours = function(bls_hours_xlsx) {
-
-# }
-
-# document_bls_wages = function(early, current) {
-#   # BLS early production workers wages: series EEU00500006
-#   # downloaded via https://data.bls.gov/series-report
-#   # bls_early_wages_csv = tar_file("data_inputs/bls_EEU00500006.csv")
-#   # # BLS series inputs for API call
-#   # bls_series_csv = tar_file("data_inputs/bls_series_codes.csv")
-# }
 
 # document_bea_data = function(bea_series_csv) {
 #   # # BEA series inputs for API call
